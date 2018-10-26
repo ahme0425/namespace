baa1847f47a123865355f5a87e4c2cf4ef25a1e2
ahme0425 <43075315+ahme0425@users.noreply.github.com>
2018-10-26 16:53:46 -0400

    new

---
+++ namespace.js
@@ -0,0 +1,23 @@
+ var AHME0425 = {
+    init: function () {
+        let stepone = document.createElement("div");
+        stepone.className = "box";
+        stepone.textContent = "AhmE0425";
+        document.getElementById("boxes").appendChild(stepone);
+
+        stepone.addEventListener("click", function () {
+            stepone.style.borderColor = "#325152";
+            stepone.style.backgroundColor = "#900938";
+        });
+     
+        stepone.addEventListener("mouseover", function () {
+            stepone.classList.toggle("highlight");
+        });
+
+        stepone.addEventListener("mouseout", function () {
+            stepone.target.classList.toggle("highlight");
+            stepone.target.removeAttribute("style");
+        });
+    
+};
+    
